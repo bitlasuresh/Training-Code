163dafb35682b77c558ca9c700c15dc5df20307f
Suresh Babu <suresh.babu@bitlasoft.com>
2019-10-04 10:47:53 +0530

    chnage

--- JS/New website/JavaScript/index.js
+++ JS/New website/JavaScript/index.js
@@ -1,1 +1,11 @@
 document.getElementById('date').innerHTML = new Date().toDateString();
+
+$.var ajax = new XMLHttpRequest();
+ajax.onreadystatechange = function() {
+	if (ajax.readyState == 4 && ajax.status == 200) {
+		var response = ajax.responseText;
+	}
+};
+ajax.open(method, URL, true);
+ajax.setRequestHeader("Content-type", "application/json");
+ajax.send(data);
---
+++ VueJS/script.js
@@ -0,0 +1,67 @@
+
+// var fruits = 90, price=100;
+
+// var companyName = fruits + price;
+
+// document.getElementById("app").innerHTML = fruits + price;
+
+// function aa(){
+
+
+// // var fruits = 90, price=100;
+
+// // 	var companyName = fruits + price;
+
+// 	var score = 80
+
+// 	score += 5
+
+// 	document.getElementById("app").innerHTML = score;
+
+// };
+
+// var fruits = 90, price=100;
+
+// var companyName = fruits + price;
+
+// 	document.getElementById('app').innerHTML = 'companyName';
+
+// var greeting = "hello";
+// 	var highscore = 400;
+// 	var myscore =450;
+// 		debugger;
+
+
+// 	if (myscore<highscore){
+// 		greeting = "Opps"
+// 	}
+// 	function aa(){
+// 		debugger;
+// 	document.getElementById('app').innerHTML = 'greeting';
+// 	}
+
+// alert("some code");
+// // "use strict" below is ignored--it must be at the top
+
+// "use strict";
+
+// strict mode is not activated
+
+// let hello = 'hello world';
+
+// let message;
+
+// message = hello;
+
+// alert(hello);
+// alert(message);
+
+// var a = 100, b = 100;
+
+// var totalSum = a + b;
+
+// function sum (){
+
+// 	document.getElementById("#app").innerHTML = a + b;
+
+// }
